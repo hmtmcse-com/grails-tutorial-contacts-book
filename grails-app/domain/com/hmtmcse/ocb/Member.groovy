2d086b67d98f9c76948d00b11cf478c25f94a8aa
Touhid Mia <hmtm.cse@gmail.com>
2019-03-21 16:44:20 +0600

    Grails CRUD Registration Form -1 (Domain), (Grails Tutorial Part-3)

---
+++ grails-app/domain/com/hmtmcse/ocb/Member.groovy
@@ -0,0 +1,35 @@
+package com.hmtmcse.ocb
+
+class Member {
+
+    Integer id
+    String firstName
+    String lastName
+    String email
+    String password
+    String memberType = GlobalConfig.USER_TYPE.REGULAR_MEMBER
+    String identityHash
+    Date identityHashLastUpdate
+    Boolean isActive = true
+
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+        email(email: true, nullable: false, unique: true, blank: false)
+        password(blank: false)
+        lastName(nullable: true)
+        identityHash(nullable: true)
+        identityHashLastUpdate(nullable: true)
+    }
+
+    def beforeInsert (){
+        this.password = this.password.encodeAsMD5()
+    }
+
+
+    def beforeUpdate(){
+        this.password = this.password.encodeAsMD5()
+    }
+
+}
---
+++ src/main/groovy/com/hmtmcse/ocb/GlobalConfig.groovy
@@ -0,0 +1,10 @@
+package com.hmtmcse.ocb
+
+class GlobalConfig {
+
+    public static final def USER_TYPE = [
+            ADMINISTRATOR: "ADMINISTRATOR",
+            REGULAR_MEMBER: "REGULAR_MEMBER",
+    ]
+
+}
